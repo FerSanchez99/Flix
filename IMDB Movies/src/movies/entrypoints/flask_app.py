33fe2e1b6f64a896c08baeacd4a923a92745c7e7
Axel Rodríguez <A01570152@itesm.mx>
2022-05-25 17:45:38 -0500

    Connection to DB, login and register

--- IMDB Movies/src/movies/entrypoints/flask_app.py
+++ IMDB Movies/src/movies/entrypoints/flask_app.py
@@ -1,14 +1,13 @@
 from flask import Flask, request, render_template, redirect
 from movies import models
-from Login import Login
+import controller as db
+
 app = Flask(__name__)
 models.start_mappers()
-
 
 @app.route("/login", methods=["GET"])
 def login():
     return render_template('login.html')
-
 
 @app.route("/signup", methods=["GET"])
 def signup():
@@ -18,15 +17,19 @@ def signup():
 def dashboard():
     return "Dashboard de Flix"
 
-
 @app.route('/auth_user', methods=['POST'])
 def auth_user():
     email = request.form['email']
     password = request.form['password']
-    return Login.auth(email, password)
+    if db.read(email, password):
+        return redirect('/dashboard') 
+    return redirect('/login')
     
 @app.route('/create_user', methods=['POST'])
 def create_user():
-    return "Usuario creado"
+    email = request.form['email']
+    password = request.form['password']
+    db.create(email, password)
+    return redirect('/login')
     
     
